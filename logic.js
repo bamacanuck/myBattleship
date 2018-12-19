017fe13cfe454a4006e6f831e785e03a1c62ca8f
Shane Scott <shaneradio@gmail.com>
2018-12-19 16:24:33 -0500

    altered our console.log silliness

--- logic.js
+++ logic.js
@@ -1,15 +1,15 @@
-console.log("hey");
-console.log("now");
+console.log("hey,");
+console.log("now...");
 
 console.log("");
-console.log("");
-console.log("");
+// console.log("");
+// console.log("");
 
-console.log("hey");
-console.log("now");
+console.log("hey,");
+console.log("now...");
 
 console.log("");
-console.log("");
-console.log("");
+// console.log("");
+// console.log("");
 
-console.log("Don't dream it's over.");
+console.log("don't dream it's over...");
