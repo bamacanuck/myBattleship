88b50bece94c6322929647fd82d5e72ff3297ffa
Shane Scott <shaneradio@gmail.com>
2018-12-19 16:41:49 -0500

    setting up guess logic

--- logic.js
+++ logic.js
@@ -2,13 +2,15 @@ let loc1 = 3;
 let loc2 = 4;
 let loc3 = 5;
 
-let guess;
-
 let hits = 0;
 
 let guesses = 0;
 
 let isSunk = false;
+
+let guess = prompt("Choose a target (enter a whole number, 1-7) !!!");
+
+
 
 // ================console.log silliness (below)================
 
